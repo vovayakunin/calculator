c322a0f5b968d9d2499a7a4cd4092cc1fefe5513
Vladimir <vovaykunin@gmail.com>
2023-05-06 22:31:58 +0300

    На проверку

---
+++ src/main/java/com/calculator/calculator/service/CalculatorService.java
@@ -0,0 +1,22 @@
+package com.calculator.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
